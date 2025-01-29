d3056b0b843440c0a2a26b32934583b833983fc1
anais <gatard.anais@gmail.com>
2025-01-29 16:17:28 +0100

    modif readme et ajout template

--- {{cookiecutter.project_slug}}/config/urls.py
+++ {{cookiecutter.project_slug}}/config/urls.py
@@ -1,28 +1,29 @@
 from django.contrib import admin
 {% if cookiecutter.use_wagtail %}
 from django.urls import path, include
-{% else %}
-from django.urls import path
-{% endif %}
-from django.views.generic import TemplateView
-{% if cookiecutter.use_wagtail %}
 from wagtail.admin import urls as wagtailadmin_urls
 from wagtail import urls as wagtail_urls
 from wagtail.documents import urls as wagtaildocs_urls
 from django.conf import settings
 from django.conf.urls.static import static
+{% else %}
+from django.urls import path
+from django.views.generic import TemplateView
 {% endif %}
 
+{% if cookiecutter.use_wagtail %}
 urlpatterns = [
     path('admin/', admin.site.urls),
-    {% if cookiecutter.use_wagtail %}
     path('cms/', include(wagtailadmin_urls)),
     path('documents/', include(wagtaildocs_urls)),
     path('', include(wagtail_urls)),
-    {% else %}
-    path('', TemplateView.as_view(template_name='index.html'), name='index'),
-    {% endif %}
 ]
+{% else %}
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', TemplateView.as_view(template_name='index.html'), name='index'),
+]
+{% endif%}
 
 {% if cookiecutter.use_wagtail %}
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
